c203e8f94dc9357adda877f7b4c38271a7c31cf8
AMartelanc <alemartelanc@gmail.com>
2023-09-12 20:37:24 -0300

    Rodapé com lista de integrantes

---
+++ src/components/Rodape.jsx
@@ -0,0 +1,27 @@
+export default function Rodape() {
+    return (
+        <>
+        <footer>
+        <div>
+          <ul>
+            <li>
+              <p>550381 - João Vitor Souza Nunes</p>
+            </li>
+            <li>
+              <p>550511 - Denise nascimento Baptistucci</p>
+            </li>
+            <li>
+              <p>99572 - Matheus Colossal Araujo</p>
+            </li>
+            <li>
+              <p>551173 - Guilherme Costa Batista</p>
+            </li>
+            <li>
+              <p>550247 - Alexandre Martelanc</p>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </>
+    )    
+}
